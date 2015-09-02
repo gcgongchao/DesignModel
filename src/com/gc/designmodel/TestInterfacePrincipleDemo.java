ba3e6bf1f13ae2c741e8d0c972aa3545fced4d39
AndroidJiangJun <AndroidJiangJun@gongchao>
2015-09-02 16:54:51 +0800

    add the class TestInterfacePrincipleDemo

---
+++ src/com/gc/designmodel/TestInterfacePrincipleDemo.java
@@ -0,0 +1,25 @@
+package com.gc.designmodel;
+/***
+ * 
+    * @说明 设计模式之接口隔离原则学习
+    * @作者 GeneralAndroid
+    * @创建日期 2015-9-2 下午3:46:24
+    * @版本号 1.0
+    * @since 1.0
+ */
+public class TestInterfacePrincipleDemo {
+	public static void main(String[] args) {
+		
+	}
+
+}
+interface IPettyGirl
+{
+	/**要有较好的面孔**/
+	public void goodLooking();
+	/**要有好身材**/
+	public void niceFigure();
+	/**要有气质**/
+	public void greatTemperament();
+}
+
